112fd42b318f178c590719ec36086eb8955a555a
BIGRIG TOKEN OPERATION <mcdonaldoneil504@gmail.com>
2025-08-01 14:52:01 -0400

    Create Georgecoin.sol

---
+++ Georgecoin.sol
@@ -0,0 +1,26 @@
+{8 items
+key:
+4
+updateAuthority:
+"B4zM28tTGSf4S9JG4WhhHxDPmPBbbihmcd1Z8NymvcwH"
+mint:
+"5LSRDygN3hLzkLJB2odkVeUGkHFVx9iADRoGqQ9Fdxxm"
+data:{4 items
+name:
+"GEORGE COIN"
+symbol:
+"COIN-OG"
+uri:
+"https://ipfs.io/ipfs/QmPfoL8A8qAtptm8PzJXn1ifhZF5HYr1eUUoHZVr37JTsC"
+sellerFeeBasisPoints:
+0
+}
+primarySaleHappened:
+0
+isMutable:
+0
+editionNonce:
+255
+tokenStandard:
+2
+}
